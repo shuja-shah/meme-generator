4d788464ade874476f45409b0a5d0806949f7f01
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-19 12:30:31 -0700

    Define Form Componenet

---
+++ src/Components/Form.js
@@ -0,0 +1,8 @@
+const MyForm = () => (
+  <Form className="generationForm">
+    <input className="input" type="text" placeholder="Upper Text" />
+    <input className="input" type="text" placeholder="Bottom Text" />
+    <input className="submit" type="submit" value="Generate The MEME" />
+  </Form>
+);
+export default MyForm;
